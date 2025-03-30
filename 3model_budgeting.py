370bab3223f8b115f02c493c454de651c92fb0cb
Ivan Radkevich <radke149@umn.edu>
2025-03-30 00:27:25 -0500

    fixed hidden_states to if hidden_states is not None
    Signed-off-by: Settheworldonfireiii <radke149@umn.edu>

--- 3model_budgeting.py
+++ 3model_budgeting.py
@@ -17,8 +17,8 @@ if __name__ == "__main__":
      
     parser = argparse.ArgumentParser()
     parser.add_argument('--model_name',  help="model name, including vendor", type= str)
-    parser.add_argument('--min_tokens',  help="minimal amount of tokens that the model must think for during each generation, whether original one or extended", nargs = '?', const = 0, default = 0, type= int)
-    parser.add_argument('--max_tokens',  help="token threshold, or maximal amount of tokens that we allow before we interrupt the model and force it to give the answer", type= int)
+    parser.add_argument('--min_tokens',  help="minimal amount of tokens that the model must think for in total", nargs = '?', const = 0, default = 0, type= int)
+    parser.add_argument('--max_tokens',  help="token threshold, or maximal amount of tokens that we allow it to think during each generation.", type= int)
     parser.add_argument('--max_final_tokens',  help="maximum amount of tokens the model is allowed to output after its thinking was interrupted and it is forced to give a final answer", nargs = '?', const = 0, default = 0, type= int)
     parser.add_argument('--temperature',  help="generation temperature", type= float)
     parser.add_argument('--num_ignore',  help="how many times to ignore end-of-thinking token", nargs = '?', const = 0, default = 0,  type= int)
@@ -31,13 +31,13 @@ if __name__ == "__main__":
     parser.add_argument('--cp_arg_2',  help="custom prompt input argument e g text, or input, or insturction, specific to dataset", nargs = '?', const = "", default = "", type= str)
     parser.add_argument('--cp_arg_3',  help="custom prompt input argument e g text, or input, or insturction, specific to dataset", nargs = '?', const = "", default = "", type= str)
     parser.add_argument('--custom_final_answer_prompt',  help="if we generate the answer separately, do we also generate it with some custom prompt?", nargs = '?', const = "\n Final Answer: ", default = "\n Final Answer", type= str)
-    parser.add_argument('--ignore_string',  help="Some custom ignore string if Wait, think again is not OK for you", nargs = '?', const = ". Wait, think again", default = ". Wait, think again", type= str)
+    parser.add_argument('--ignore_string',  help="Some custom ignore string if Wait, think again is not OK for you", nargs = '?', const = ". Wait, think again", default = "Now, check your answer again ", type= str)
     
 
     args=parser.parse_args()
     # Decide on a token limit for thinking; As the model's max tokens is 32768, 32000 usually ensures there is enough space for the model to still answer
     # Decide how often to ignore end-of-thinking token
-    NUM_IGNORE = args.num_ignore
+    
 
 
 
@@ -61,7 +61,7 @@ if __name__ == "__main__":
     data = []
     for i, p in enumerate(prompts):
         if args.custom_prompt:
-            prompt += args.custom_prompt
+            prompt = args.custom_prompt
             if args.cp_arg_1:
                 prompt += p[args.cp_arg_1]
                 if args.cp_arg_2:
@@ -76,17 +76,21 @@ if __name__ == "__main__":
                 prompt = "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n" + p['prompt'] + "<|im_end|>\n<|im_start|>assistant\n"
             elif args.dataset == "bespokelabs/Bespoke-Stratos-17k":
                 prompt = "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant. Your role as an assistant involves thoroughly exploring questions through a systematic long thinking process before providing the final precise and accurate solutions. This requires engaging in a comprehensive cycle of analysis, summarizing, exploration, reassessment, reflection, backtracing, and iteration to develop well-considered thinking process.  Each step should include detailed considerations such as analisying questions, summarizing relevant findings, brainstorming new ideas, verifying the accuracy of the current steps, refining any errors, and revisiting previous steps. <|im_end|>\n<|im_start|>user\n" + p['conversations'][0]['value'] +  "<|im_end|>\n<|im_start|>assistant\n"
-            #prompt = "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n" + p['conversations'][0]['value'] + "<|im_end|>\n<|im_start|>assistant\n"
+                # or, you can discard the prompt above that was taken from bespoke dataset official page and use the official QWen prompt from below, adapted for bespoke's column names and data structure.
+                #prompt = "<|im_start|>system\nYou are Qwen, created by Alibaba Cloud. You are a helpful assistant.<|im_end|>\n<|im_start|>user\n" + p['conversations'][0]['value'] + "<|im_end|>\n<|im_start|>assistant\n"
+        """
+        For R1-based models (original DeepSeek, not distilled from QWen), it is better to adopt the following template
+        A conversation between User and Assistant. The User asks a question, and the Assistant solves it. The Assistant first thinks about the reasoning process in the mind and then provides the User with the answer.
+        The reasoning process is enclosed within <think> </think> and answer is enclosed within <answer> </answer> tags, respectively, i.e., <think> reasoning process here </think> <answer> answer here </answer>.
+        \nUser: {question}\nAssistant: <think>
 
+
+        also some people have proposed <|begin_of_thought|> and <|end_of+thought|>
+        """
+        NUM_IGNORE = args.num_ignore
         original_prompt = prompt
         orpleen  = len(original_prompt)
         stop_token_ids = tok("<|im_start|><|im_end|>")["input_ids"]
-        """
-        if args.dataset == "bespokelabs/Bespoke-Stratos-17k":
-            stop_token_ids = tok("<|begin_of_thought|> <|end_of_thought|>")["input_ids"]
-        else:
-            stop_token_ids = tok("<|im_start|><|im_end|>")["input_ids"]
-        """
         sampling_params = SamplingParams(
             max_tokens=args.max_tokens,
             stop_token_ids=stop_token_ids,
@@ -94,29 +98,19 @@ if __name__ == "__main__":
             temperature=args.temperature,
             top_p = args.topp
         )
-        """
-        if args.dataset == "bespokelabs/Bespoke-Stratos-17k":
-            prompt += "<|begin_of_thought|> "
-        else:
-            prompt += "<|im_start|>think"
-        """
-        prompt += "<|im_start|>think"
+        
         o = model.generate(
             prompt,
             sampling_params=sampling_params
         )
          
         max_tokens_thinking_tmp = args.min_tokens
-        print("=====================FIRST==========================================")
-        print(o[0].outputs[0].text)
-        # Num of times to skip stop token
         if args.mode == "lax":          
             while ((NUM_IGNORE>0) and (max_tokens_thinking_tmp > 100)):
-                print("ignore ", NUM_IGNORE)
                 max_tokens_thinking_tmp -= len(o[0].outputs[0].token_ids)
                 prompt += o[0].outputs[0].text + args.ignore_string
                 sampling_params = SamplingParams(
-                    max_tokens= max(max_tokens_thinking_tmp, 400),
+                    min_tokens= max(max_tokens_thinking_tmp, 400),
                     stop_token_ids=stop_token_ids,
                     skip_special_tokens=False,
                     temperature=args.temperature,
@@ -131,11 +125,10 @@ if __name__ == "__main__":
                 NUM_IGNORE -= 1
         else:
             while ((NUM_IGNORE>0) or (max_tokens_thinking_tmp > 100)):
-                print("ignore ", NUM_IGNORE)
                 max_tokens_thinking_tmp -= len(o[0].outputs[0].token_ids)
                 prompt += o[0].outputs[0].text + args.ignore_string
                 sampling_params = SamplingParams(
-                    max_tokens= max(max_tokens_thinking_tmp, 400),
+                    min_tokens= max(max_tokens_thinking_tmp, 400),
                     stop_token_ids=stop_token_ids,
                     skip_special_tokens=False,
                     temperature=args.temperature,
@@ -153,17 +146,10 @@ if __name__ == "__main__":
         prompt += o[0].outputs[0].text + args.custom_final_answer_prompt # You can also append "Final Answer:" here like we do for some evaluations to prevent the model from just continuing to reason in its answer when early exiting
         
         
-        """
-        if args.dataset == "bespokelabs/Bespoke-Stratos-17k":
-            prompt += "<|im_start|>system\n Now, give the final answer, based on various attempts, explorations, and reflections from the chain of thought you previously generated, systematically present the final solution that you deem correct. The solution should have a logical, accurate, concise expression style and detail necessary steps needed to reach the conclusion.<|im_end|>"
-            stop_token_ids = tok("<|end_of_solution|>")["input_ids"]
-        else: 
-            stop_token_ids = tok("<|im_end|>")["input_ids"]
-        """
         if args.max_final_tokens > 0:
             stop_token_ids = tok("<|im_end|>")["input_ids"]
             sampling_params = SamplingParams(
-                max_tokens=args.max_final_tokens,
+                min_tokens=args.max_final_tokens,
                 stop_token_ids=stop_token_ids,
                 skip_special_tokens=False,
                 temperature=args.temperature,
@@ -174,7 +160,7 @@ if __name__ == "__main__":
                 prompt,
                 sampling_params=sampling_params,
             )
-        print("With budget forcing:") # You will see that after the "Wait" in the reasoning trace it fixes its answer
+
         if args.dataset == "bespokelabs/Bespoke-Stratos-17k":
                       
             print("==========================+ANSWER END+==========")
